b977ea3db9fde7a7f2696d732c924b6e8f1b9fb4
niloofar soleimani <soleimaniniloofar95@gmail.com>
2026-08-24 12:29:33 +0330

    made the task details

---
+++ src/Component/Switch/Switch.tsx
@@ -0,0 +1,76 @@
+type SwitchProps = {
+  checked: boolean;
+  disabled?: boolean;
+  onClick?: (checked: boolean) => void;
+  readOnly?: boolean;
+};
+
+export function Switch({
+  checked = false,
+  disabled = false,
+  onClick,
+  readOnly = false,
+}: SwitchProps) {
+  const containerStyle = calculateContainerStyle(checked, disabled, readOnly);
+
+  const handleStyle = calculateHandleStyle(checked, disabled);
+
+  const Tag = readOnly ? "div" : "button";
+
+  const handleClick = () => {
+    if (!disabled && !readOnly) {
+      onClick?.(!checked);
+    }
+  };
+
+  return (
+    <Tag
+      className={containerStyle}
+      {...(!readOnly
+        ? {
+            type: "button",
+            disabled,
+            onClick: handleClick,
+          }
+        : {})}
+    >
+      <div className={handleStyle} />
+    </Tag>
+  );
+}
+
+function calculateContainerStyle(
+  checked: boolean,
+  disabled: boolean,
+  readOnly: boolean
+) {
+  const baseStyle = "rounded-full border-2 w-14 h-8 items-center flex";
+
+  const onStyle = "bg-orange-500 border-orange-500 justify-end";
+
+  const offStyle = "bg-stone-300 border-stone-500 p-1 justify-start";
+
+  let finalStyle = baseStyle;
+
+  if (!readOnly) {
+    finalStyle += " cursor-pointer";
+  }
+
+  finalStyle += checked ? ` ${onStyle}` : ` ${offStyle}`;
+
+  if (disabled) {
+    finalStyle += " opacity-50";
+  }
+
+  return finalStyle;
+}
+
+function calculateHandleStyle(checked: boolean, disabled: boolean) {
+  const baseStyle = "m-1 rounded-full";
+
+  const onStyle = "size-6 bg-stone-50";
+
+  const offStyle = "size-4 bg-stone-500";
+
+  return `${baseStyle} ${checked ? onStyle : offStyle}`;
+}
--- src/routes/task.tsx
+++ src/routes/task.tsx
@@ -1,4 +1,4 @@
-import { CircleIcon, StarIcon } from "@phosphor-icons/react";
+import { CircleIcon, PencilSimpleIcon, StarIcon, TrashIcon, UserSwitchIcon } from "@phosphor-icons/react";
 import { CheckCircleIcon } from "@phosphor-icons/react/dist/ssr/CheckCircle";
 import { createFileRoute } from "@tanstack/react-router";
 import { use, useState } from "react";
@@ -9,6 +9,9 @@ import { BottomBar } from "../Component/BottomBar/BottomBar";
 import { StarContext } from "../Component/shared/star.contex";
 import { type Task, TasksContext } from "../Component/shared/tasks.context";
 import { TopBar } from "../Component/TopBar/TopBar";
+import { BottomSheet } from "../Component/Sheet/Sheet";
+import { Btn } from "../Component/Btn/Btn";
+import { Switch } from "../Component/Switch/Switch";
 
 export const Route = createFileRoute("/task")({
 	component: RouteComponent,
@@ -65,6 +68,7 @@ type TaskItemProps = {
 function TaskItem({ id, title, isCompleted }: TaskItemProps) {
 	const { toggleTaskCompleted } = use(TasksContext);
 	const { startedTaskId, setStartedTaskId } = use(StarContext);
+	const [isTaskDetailsSheetOpen,setTaskDetailsSheetOpen]=useState(false)
 	const IsCopmpletedIcon = isCompleted ? CheckCircleIcon : CircleIcon;
 	const isThisTaskStared = startedTaskId === id;
 	const handleCompletedBtnClick = () => {
@@ -73,28 +77,80 @@ function TaskItem({ id, title, isCompleted }: TaskItemProps) {
 	const handleStarBtnClick = () => {
 		return setStartedTaskId(isCompleted ? "" : isThisTaskStared ? "" : id);
 	};
+	 const closeTaskDetailsSheet=()=>setTaskDetailsSheetOpen(false)
+	 const OpenTaskDetailsSheet=()=>setTaskDetailsSheetOpen(true)
 	return (
-		<div
-			className={`flex w-full h-12 ${isCompleted ? "text-stone-600 line-through" : "text-stone-900"} `}
-		>
-			<button type="button" onClick={handleCompletedBtnClick}>
-				<IsCopmpletedIcon size={24} />
-			</button>
+    <>
+      {isTaskDetailsSheetOpen && (
+        <TaskDetailsSheet onClose={closeTaskDetailsSheet} id={id} title={title} isCompleted={isCompleted} />
+      )}
 
-			<button
-				type="button"
-				className={`cursor-pointer flex-1 text-start px-3 ${isCompleted}`}
-			>
-				{title}
-			</button>
+      <div
+        className={`flex w-full h-12 ${isCompleted ? "text-stone-600 line-through" : "text-stone-900"} `}
+      >
+        <button type="button" onClick={handleCompletedBtnClick}>
+          <IsCopmpletedIcon size={24} />
+        </button>
 
-			<button
-				type="button"
-				className={`cursor-pointer p-3 ${isThisTaskStared ? "text-orange-500" : null}`}
-				onClick={handleStarBtnClick}
-			>
-				<StarIcon size={24} weight={isThisTaskStared ? "fill" : "regular"} />
-			</button>
-		</div>
-	);
+        <button
+          type="button"
+		  onClick={OpenTaskDetailsSheet}
+          className={`cursor-pointer flex-1 text-start px-3 ${isCompleted}`}
+        >
+          {title}
+        </button>
+
+        <button
+          type="button"
+          className={`cursor-pointer p-3 ${isThisTaskStared ? "text-orange-500" : null}`}
+          onClick={handleStarBtnClick}
+        >
+          <StarIcon size={24} weight={isThisTaskStared ? "fill" : "regular"} />
+        </button>
+      </div>
+    </>
+  );
 }
+type TaskDetailsSheetProps={
+	id:Task['id'],
+	title:Task['title'],
+	isCompleted:Task['isCompleted']
+	onClose:()=>void
+}
+
+  function TaskDetailsSheet({ onClose , id , title, isCompleted }: TaskDetailsSheetProps) {
+	const { toggleTaskCompleted } = use(TasksContext);
+	// const [isOn , setIsOn]=useState(false)
+	 const handleToggleClick=()=>toggleTaskCompleted(id)
+    return (
+      <BottomSheet
+        onClose={onClose}
+        topBar={<TopBar title=" مشخصات" onCloseBtnClick={onClose} />}
+      >
+        <p className="text-stone-900 font-bold text-2xl text-center">{title}</p>
+        <div className="flex flex-col gap-2">
+          <button
+            onClick={handleToggleClick}
+            type="button"
+            className="p-2 ps-4 flex items-center justify-between border-2 border-dashed border-stone-300 rounded-full  text-stone-900 font-bold"
+          >
+            {" "}
+            وضعیت :
+            <Switch readOnly checked={isCompleted} />
+          </button>
+          <Btn
+            title=" ویرایش"
+            IconEnd={PencilSimpleIcon}
+            style="light"
+            color="neutral"
+          />
+          <Btn
+            title=" حدف کن"
+            IconEnd={TrashIcon}
+            color="danger"
+            style="light"
+          />
+        </div>
+      </BottomSheet>
+    );
+  }
